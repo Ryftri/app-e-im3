b4e402494a830a1a1dba11fcaa819de161898f88
Ryftri <rayhanzulfitri@gmail.com>
2024-10-17 02:30:51 +0200

    update(footer): update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export function FooterComponent () {
     return (
         <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 Aplikasi <Link href={"/"} className="hover:underline">E-iM3</Link></span>
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© Hibah Pemberdayaan Berbasis Masyarakat Kemdikbud 2024 - Universitas PGRI Madiun 2024</span>
             </div>
         </footer>
     )
